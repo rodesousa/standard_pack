5b3255240f0168ee35ee3c581ce4ef46e748059b
rodesousa <dessroberto@gmail.com>
2015-12-14 17:04:01 +0100

    move controller editor

--- standardController/src/main/scala/controller/editor/controller/ControllerEditor.scala
+++ standardController/src/main/scala/controller/editor/controller/ControllerEditor.scala
@@ -1,12 +1,11 @@
-package controller.controller
+package controller.editor.controller
 
 import controller.standard.Controller
 import editor.model.ModelEditor
+import standard.model.event.StructEvent
 
 class ControllerEditor(_model: ModelEditor) extends Controller(_model) {
   val model = _model
-
-  val pipeEvent = ???
 
   val controllerFight = ???
 
