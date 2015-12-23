990ca9652c857e7ce678ec2f7c2e2ece3c3153f1
rodesousa <dessroberto@gmail.com>
2015-12-23 15:07:20 +0100

    refacto

--- standardLauncher/src/main/scala/warrior/launcher/Launcher.scala
+++ standardLauncher/src/main/scala/warrior/launcher/Launcher.scala
@@ -1,8 +1,10 @@
 package warrior.launcher
 
 import controller.warrior.ControllerWarrior
+import standard.resources.Variables
 import view.warrior.ViewWarrior
 import warrior.example.Persos
+import warrior.map.MapWarrior
 import warrior.model.ModelWarrior
 
 /**
@@ -10,9 +12,13 @@ import warrior.model.ModelWarrior
  */
 object Launcher {
   def main(args: Array[String]) {
+    // init model
     val model = new ModelWarrior
+    model.changeMap(new MapWarrior(Variables.MAP_NAME))
+    model.changePerso(Persos.createPersoPri)
+
     Persos.PnjTOEvent(model)
     val controller = new ControllerWarrior(model)
-    val view = new ViewWarrior(controller)
+    new ViewWarrior(controller)
   }
 }
