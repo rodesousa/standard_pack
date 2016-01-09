f6f0b790f5651ae7f42050a941832848d6af88d0
rodesousa <dessroberto@gmail.com>
2016-01-09 17:43:28 +0100

    ajout pnj

--- standardModel/src/main/scala/standard/model/perso/job/Pnj.scala
+++ standardModel/src/main/scala/standard/model/perso/job/Pnj.scala
@@ -1,8 +1,11 @@
 package standard.model.perso.job
 
+import standard.model.event.{HaveEvent, Events}
+import standard.model.perso.Personnage
+
 /**
  * * Created by rds on 13/05/15.
  */
-class Pnj(_name : String) {
+abstract class Pnj(_name: String) extends Personnage(_name) with Events{
   var name = _name
 }
